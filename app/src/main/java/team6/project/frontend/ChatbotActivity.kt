4c4c2ca27588fd2bff3a76769fb63f4a504b3b71
Jess Leatherland <jleatherland1@sheffield.ac.uk>
2023-03-15 14:27:23 +0000

    Added down arrow image to Retract Chatbot Screen button

--- app/src/main/java/team6/project/frontend/ChatbotActivity.kt
+++ app/src/main/java/team6/project/frontend/ChatbotActivity.kt
@@ -4,18 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import team6.project.frontend.theme.AugRealityAIArtTheme
+import team6.project.R
 
 class ChatbotActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,7 +51,10 @@ fun ChatbotScreen(toCameraScreen: () -> Unit, modifier: Modifier = Modifier) {
 
 @Composable
 fun RetractScreenButton(onClick: () -> Unit) {
-    Button(onClick = onClick) {
-        Text(text = "To Camera Screen")
-    }
+    val image = painterResource(id = R.drawable.keyboard_down_arrow)
+    Image(
+        painter = image,
+        contentDescription = "To Camera Screen",
+        modifier = Modifier.clickable { onClick() }
+    )
 }
