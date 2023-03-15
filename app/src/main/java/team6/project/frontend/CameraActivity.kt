dba4c38c633f26c7f108c06919bb5e9c3f14287e
Jess Leatherland <jleatherland1@sheffield.ac.uk>
2023-03-15 10:47:43 +0000

    Added the camera screen file (with template code)

---
+++ app/src/main/java/team6/project/frontend/CameraActivity.kt
@@ -0,0 +1,30 @@
+package team6.project.frontend
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import team6.project.frontend.theme.AugRealityAIArtTheme
+
+class CameraActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AugRealityAIArtTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
