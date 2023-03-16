49bbad2255ccfeb5cf5d45b908a7a4f82e537e2c
Jess Leatherland <jleatherland1@sheffield.ac.uk>
2023-03-16 20:27:24 +0000

    Merged CameraActivity into MainActivity (MainActivity will now be the Camera screen with a consent popup) and updated manifest file

--- app/src/main/java/team6/project/frontend/MainActivity.kt
+++ app/src/main/java/team6/project/frontend/MainActivity.kt
@@ -1,18 +1,32 @@
 package team6.project.frontend
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
 import androidx.compose.ui.tooling.preview.Preview
 import team6.project.frontend.theme.AugRealityAIArtTheme
+import team6.project.R
 
 class MainActivity : ComponentActivity() {
+    private val REQUEST_CAMERA_PERMISSION = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,12 +36,79 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CameraScreen({ startChatbotActivity() })
                 }
             }
         }
     }
+
+    private fun requestCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CAMERA_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Camera permission granted
+                } else {
+                    // Camera permission not granted, display a static image
+                    setContent {
+                        AugRealityAIArtTheme {
+                            Surface(
+                                modifier = Modifier.fillMaxSize(),
+                                color = MaterialTheme.colors.background
+                            ) {
+                                StaticImageScreen()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun startChatbotActivity() {
+        val intent = Intent(this, ChatbotActivity::class.java)
+        startActivity(intent)
+        overridePendingTransition(R.anim.slide_in_bottom, R.anim.no_animation)
+    }
 }
+
+@Composable
+fun CameraScreen(toChatbotScreen: () -> Unit, modifier: Modifier = Modifier) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Greeting("Camera Screen")
+        ChatbotScreenButton(onClick = { toChatbotScreen() })
+    }
+}
+
+@Composable
+fun ChatbotScreenButton(onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text(text = "Talk to the painting")
+    }
+}
+
+@Composable
+fun StaticImageScreen() {
+//    Box(modifier = Modifier.fillMaxSize()) {
+//        val staticImage = painterResource(R.drawable.static_image) //Replace R.drawable.static_image with the resource ID of the image we will use
+//        Image(
+//            painter = staticImage,
+//            contentDescription = "Static image",
+//            modifier = Modifier.fillMaxSize()
+//        )
+//    }
+}
+
 
 @Composable
 fun Greeting(name: String) {
@@ -38,6 +119,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     AugRealityAIArtTheme {
-        Greeting("Android")
+        CameraScreen(toChatbotScreen = {})
     }
 }
