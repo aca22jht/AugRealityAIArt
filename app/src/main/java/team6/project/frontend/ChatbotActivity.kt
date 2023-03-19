4181ff865fff93f534d233da65b30231ca035568
Jess Leatherland <jleatherland1@sheffield.ac.uk>
2023-03-19 20:50:56 +0000

    Added IBM Watson Assistant Webview to Chatbot Screen

--- app/src/main/java/team6/project/frontend/ChatbotActivity.kt
+++ app/src/main/java/team6/project/frontend/ChatbotActivity.kt
@@ -1,19 +1,26 @@
 package team6.project.frontend
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import team6.project.R
 import team6.project.frontend.theme.AugRealityAIArtTheme
 
@@ -40,18 +47,17 @@ class ChatbotActivity : ComponentActivity() {
 
 @Composable
 fun ChatbotScreen(toCameraScreen: () -> Unit, modifier: Modifier = Modifier) {
-    Column (
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    Box {
+        ChatbotWebView("file:///android_asset/chatbot.html")
         Row (
             horizontalArrangement = Arrangement.End,
-            modifier = Modifier.fillMaxWidth().height(50.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp)
+                .padding(top = 5.dp)
         ) {
             RetractScreenButton(onClick = { toCameraScreen() })
         }
-        Greeting("Camera Screen")
     }
 }
 
@@ -62,5 +68,34 @@ fun RetractScreenButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
         contentDescription = "To Camera Screen",
         contentScale = ContentScale.Fit,
         modifier = Modifier.clickable { onClick() }
+    )
+}
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun ChatbotWebView(url: String){
+    // Adding a WebView inside AndroidView
+    // with layout as full screen
+    AndroidView(
+        factory = {
+            WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = WebViewClient()
+
+                // to play video on a web view
+                settings.javaScriptEnabled = true
+
+                // to verify that the client requesting your web page is actually your Android app.
+                settings.userAgentString = System.getProperty("http.agent")
+
+                loadUrl(url)
+            }
+        },
+        update = {
+            it.loadUrl(url)
+        }
     )
 }
