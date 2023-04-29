d6f89f2b31ea59fa1b507aa0fee334e2b54d77c8
Zongyang Cai <zcai19@sheffield.ac.uk>
2023-04-29 17:01:19 +0100

    Completed the image detection, add the stored image database, and reserve a comment for AR implementation effect.

--- app/src/main/java/team6/project/frontend/PaintingActivity.kt
+++ app/src/main/java/team6/project/frontend/PaintingActivity.kt
@@ -1,234 +1,203 @@
 package team6.project.frontend
 
-import android.Manifest
-import android.content.ContentValues.TAG
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.content.res.Resources
-import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
-import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
-import com.google.ar.core.ArCoreApk
-import com.google.ar.core.AugmentedImageDatabase
-import com.google.ar.core.Config
-import com.google.ar.core.Session
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentOnAttachListener
+import com.google.ar.core.*
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Sceneform
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.BaseArFragment.OnSessionConfigurationListener
+import com.google.ar.sceneform.ux.InstructionsController
+import com.google.ar.sceneform.ux.TransformableNode
+import kotlinx.android.synthetic.main.activity_painting.*
 import team6.project.R
-import team6.project.frontend.theme.AugRealityAIArtTheme
+import java.io.IOException
+import java.util.*
 
-class PaintingActivity : ComponentActivity() {
+
+class PaintingActivity : AppCompatActivity(), FragmentOnAttachListener,
+    OnSessionConfigurationListener {
+
+    private val TAG = "PaintingActivity"
+
+    private lateinit var mArFragment: ArFragment
+    private lateinit var mImageDatabase: AugmentedImageDatabase
+
+    private lateinit var mArButton: Button
+    private lateinit var mChatButton: Button
+
+//    private var mGirlDetected: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_painting)
 
-        // Set up theme and add PaintingScreen composables
-        setContent {
-            AugRealityAIArtTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    PaintingScreen({ startChatbotActivity() })
-                }
+        mArButton = findViewById(R.id.arButton)
+        mChatButton = findViewById(R.id.chatButton)
+
+        mArButton.setOnClickListener {
+            Toast.makeText(
+                this@PaintingActivity,
+                "Add the 3d effect of ar",
+                Toast.LENGTH_SHORT
+            ).show()
+            TODO("Add the 3d effect of ar")
+        }
+
+        mChatButton.setOnClickListener {
+            // Switch from the Painting Screen to the Chatbot Screen
+            startActivity(Intent(this, ChatbotActivity::class.java))
+            overridePendingTransition(R.anim.slide_in_bottom, R.anim.fade_out)
+            finish()
+        }
+
+        supportFragmentManager.addFragmentOnAttachListener(this)
+
+        if (savedInstanceState == null) {
+            if (Sceneform.isSupported(this)) {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.fragmentContainerView, ArFragment::class.java, null, "ar")
+                    .commit()
             }
         }
     }
 
-    // Switch from the Painting Screen to the Chatbot Screen
-    fun startChatbotActivity() {
-        val intent = Intent(this, ChatbotActivity::class.java)
-        startActivity(intent)
-        overridePendingTransition(R.anim.slide_in_bottom, R.anim.fade_out)
-        finish()
+
+    override fun onAttachFragment(fragmentManager: FragmentManager, fragment: Fragment) {
+        if (TextUtils.equals(fragment.tag, "ar") && fragment.id == R.id.fragmentContainerView) {
+            mArFragment = fragment as ArFragment
+            mArFragment.setOnSessionConfigurationListener(this)
+        }
     }
 
-    fun onSessionConfiguration(session: Session?, config: Config) {
-        // Disable plane detection
-        config.setPlaneFindingMode(Config.PlaneFindingMode.DISABLED)
-        // Images to be detected by our AR need to be added in AugmentedImageDatabase
-        val resources: Resources = this.resources
-        val database = AugmentedImageDatabase(session)
-        val painting = BitmapFactory.decodeResource(resources, R.drawable.blue)
-        database.addImage("blue", painting)
-        config.setAugmentedImageDatabase(database)
+    private var mLastToastTime: Long = 0
+
+    override fun onSessionConfiguration(session: Session, config: Config) {
+        with(config) {
+            // Disable plane detection
+            planeFindingMode = Config.PlaneFindingMode.DISABLED
+
+            focusMode = Config.FocusMode.AUTO
+
+            // Images to be detected by our AR need to be added in AugmentedImageDatabase
+            // This is how database is created at runtime
+            // You can also prebuild database in you computer and load it directly (see: https://developers.google.com/ar/develop/java/augmented-images/guide#database)
+            try {
+                assets.open("myimages.imgdb").use {
+                    mImageDatabase = AugmentedImageDatabase.deserialize(session, it)
+                }
+            } catch (e: IOException) {
+                Log.e(TAG, "IO exception loading augmented image database.", e)
+            }
+            updateMode = Config.UpdateMode.LATEST_CAMERA_IMAGE
+            // This is an alternative way to initialize an AugmentedImageDatabase instance,
+            // load a pre-existing augmented image database.
+            setAugmentedImageDatabase(mImageDatabase)
+        }
+
+        session.configure(config)
 
         // Check for image detection
-        //arFragment.setOnAugmentedImageUpdateListener(this::onAugmentedImageTrackingUpdate)
-    }
-}
+        mArFragment.setOnAugmentedImageUpdateListener { augmentedImage: AugmentedImage ->
+            Log.i(TAG, "onSessionConfiguration: ")
+//            if (mGirlDetected) {
+//                return@setOnAugmentedImageUpdateListener
+//            }
+            arButton.isEnabled = with(augmentedImage) {
+                when {
+                    trackingState == TrackingState.TRACKING && trackingMethod == AugmentedImage.TrackingMethod.FULL_TRACKING -> {
+                        // Create an anchor for the image and place the anchor.
+                        val augmentedImageName = augmentedImage.name
+                        Log.d(TAG, "Tracking Image name == $augmentedImageName")
 
+                        !TextUtils.isEmpty(augmentedImageName) && augmentedImageName.contains("girl_with_a_blue_ribbon")
 
-// Assemble all elements on the Painting Screen
-@Composable
-fun PaintingScreen(toChatbotScreen: () -> Unit, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-    val cameraPermissions = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+//                        if (!TextUtils.isEmpty(augmentedImageName) && augmentedImageName.contains("girl_with_a_blue_ribbon")) {
+//                            TODO("Add the 3d effect of ar")
+//                            AnchorNode(augmentedImage.createAnchor(augmentedImage.centerPose)).apply {
+//                                worldScale = Vector3(0.1f, 0.1f, 0.1f)
+//                                parent = mArFragment.arSceneView.scene
+//                                // Add your own logic here to render a 3D object on top of the image.
+//                                ModelRenderable.builder()
+//                                    .setSource(
+//                                        this@PaintingActivity,
+//                                        Uri.parse("models/girlWithTheBlueRibbon.glb")
+//                                    )
+//                                    .setIsFilamentGltf(true)
+//                                    .build()
+//                                    .thenAccept { model: ModelRenderable? ->
+//                                        addChild(
+//                                            TransformableNode(mArFragment.transformationSystem)
+//                                                .apply {
+//                                                    renderable = model
+//                                                })
+//                                    }
+//                                    .exceptionally { throwable: Throwable? ->
+//                                        Toast.makeText(
+//                                            this@PaintingActivity,
+//                                            "Unable to load rabbit model",
+//                                            Toast.LENGTH_LONG
+//                                        ).show()
+//                                        throwable?.printStackTrace()
+//                                        null
+//                                    }
+//                            }
 
-    var augRealityOn by remember { mutableStateOf(false) }
-
-    Box (
-        modifier = Modifier.fillMaxSize()
-    ) {
-        // If the user has granted camera access, show camera view
-        // Otherwise, show static image of painting
-        if (cameraPermissions == PackageManager.PERMISSION_GRANTED) {
-            CameraView()
-        } else {
-            StaticPaintingImage()
-        }
-
-        // If the AR button is on, overlay the AR Animation
-        if (augRealityOn) {
-            // TODO: replace this placeholder
-            Box (
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .background(Color.White)
-                    .padding(top = 5.dp, bottom = 5.dp, start = 5.dp, end = 5.dp)
-            ) {
-                ARAnimation()
-            }
-        }
-
-        // Overlay the buttons
-        Column (
-            verticalArrangement = Arrangement.SpaceBetween,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 10.dp, bottom = 16.dp)
-        ) {
-            Row (
-                horizontalArrangement = Arrangement.End,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(end = 16.dp)
-            ) {
-                ARButton { augRealityOn = !augRealityOn }
-            }
-            ChatbotScreenButton(onClick = { toChatbotScreen() })
-        }
-    }
-}
-
-
-// Display the camera view
-@Composable
-fun CameraView(
-    modifier: Modifier = Modifier,
-    cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA,
-    scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER
-) {
-    val lifecycleOwner = LocalLifecycleOwner.current
-    AndroidView(
-        modifier = modifier.semantics { contentDescription = "Camera View" },
-        factory = { context ->
-            val previewView = PreviewView(context).apply {
-                this.scaleType = scaleType
-                layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                // Preview is incorrectly scaled in Compose on some devices without this
-                implementationMode = PreviewView.ImplementationMode.COMPATIBLE
-            }
-
-            val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
-
-            cameraProviderFuture.addListener({
-                val cameraProvider = cameraProviderFuture.get()
-
-                // Preview
-                val preview = Preview.Builder()
-                    .build()
-                    .also {
-                        it.setSurfaceProvider(previewView.surfaceProvider)
+//                        } else {
+//                        }
+                    }
+                    trackingState == TrackingState.PAUSED -> {
+                        // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
+                        // but not yet tracked.  This can happen when the image is first found but the
+                        // camera has not moved enough to establish full tracking.  In this case, update the
+                        // state to detect it.
+                        Log.d(TAG, "Detected Image ${augmentedImage.name}")
+                        false
+                    }
+                    trackingState == TrackingState.STOPPED -> {
+                        Log.d(TAG, "Stopped Tracking Image ${augmentedImage.name}")
+                        // Remove anchors for AugmentedImages that are no longer tracking.
+//                        scene.removeChild(scene.findByName(augmentedImage.name))
+                        false
+                    }
+                    else -> {
+                        Log.i(TAG, "onSessionConfiguration: else")
+                        false
+                    }
+                }
+            }.also {
+                if (it) {
+                    if (System.currentTimeMillis() - mLastToastTime >= 5 * 1000) {
+                        mLastToastTime = System.currentTimeMillis()
+                        Toast.makeText(
+                            this@PaintingActivity,
+                            "ar effects can be loaded",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
 
-                try {
-                    // Must unbind the use-cases before rebinding them.
-                    cameraProvider.unbindAll()
-
-                    cameraProvider.bindToLifecycle(
-                        lifecycleOwner, cameraSelector, preview
-                    )
-                } catch (exc: Exception) {
-                    Log.e(TAG, "Use case binding failed", exc)
                 }
-            }, ContextCompat.getMainExecutor(context))
+            }
 
-            previewView
-        })
-}
-
-
-// Display AR on/off button
-@Composable
-fun ARButton(onClick: () -> Unit) {
-    val availability = ArCoreApk.getInstance().checkAvailability(LocalContext.current)
-
-    Button (
-        onClick = onClick,
-        enabled = availability.isSupported
-    ) {
-        Text(text = "AR")
+            mArFragment.instructionsController.setEnabled(
+                InstructionsController.TYPE_AUGMENTED_IMAGE_SCAN, false
+            )
+        }
     }
+
 }
 
 
-// Display AR animation
-@Composable
-fun ARAnimation() {
-    // TODO: replace with AR implementation
-    Text(text = "Need to add AR")
-}
-
-
-// Display a static image of the painting
-@Composable
-fun StaticPaintingImage() {
-    val paintingImage = painterResource(R.drawable.girl_with_a_blue_ribbon)
-    Image(
-        painter = paintingImage,
-        contentDescription = "The girl with a blue ribbon painting",
-        contentScale = ContentScale.Crop,
-        modifier = Modifier.fillMaxSize()
-    )
-}
-
-
-// Display the button for navigating to the Chatbot Screen
-@Composable
-fun ChatbotScreenButton(onClick: () -> Unit) {
-    Button(onClick = onClick) {
-        Text(text = "Talk to the painting")
-    }
-}
 
 
